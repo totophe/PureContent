f24ce14480ebddc51a8f7ccd72a2ea9c3458202a
Jean-Christophe Cuvelier <jcc@artack.ch>
2015-04-15 16:25:52 +0200

    Initial proof of concept

---
+++ README.md
@@ -0,0 +1,35 @@
+# PureContent
+
+PureContent is a light static "CMS" system allows you to easily build and maintain a static website
+
+PureContent uses Nunjucks for it's templating system, but you are free to adapt it to any other templating system you would like.
+
+At the moment, PureContent is more a proof of concept than anything else, but feel free to contribute.
+
+## Install
+
+To install the system, run the following command:
+
+```npm install```
+
+## Development
+
+When you start developing your website, you can launch the following command:
+
+```npm start```
+
+This will launch a web server and track your changes to update it live
+
+The ./src folder should contain at least this a /templates folder and the /app/config/parameters.json file.
+
+## Production
+
+NOT IMPLEMENTED YET (You can just use the result from the /web though)
+
+When you are ready to build the production version, just use:
+
+```npm build``` 
+
+It will generate the static "production" version of your website that you can upload online
+
+
---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var watch = require('gulp-watch');
+var source = require('vinyl-source-stream');
+var rename = require('gulp-rename');
+var fs = require('fs');
+var data = require('gulp-data');
+
+var handlebars = require('gulp-compile-handlebars');
+var nunjucksRender = require('gulp-nunjucks-render');
+
+gulp.task('server', function () {
+	browserSync({
+		server: {
+			baseDir: "./web"
+		}
+	});
+});
+
+//gulp.task('browserify', function () {
+//	return browserify('./src/js/app.js')
+//		.bundle()
+//		//Pass desired output filename to vinyl-source-stream
+//		.pipe(source('pictawall.js'))
+//		// Start piping stream to tasks!
+//		.pipe(gulp.dest('./web/lib/js'))
+//		.pipe(browserSync.reload({stream: true}));
+//});
+
+gulp.task('templates', function () {
+	nunjucksRender.nunjucks.configure(['./src/templates/']);
+	return gulp.src('./src/templates/*.html')
+		.pipe(data(getDataForFile))
+		.pipe(nunjucksRender())
+		.on('error', handleError)
+		.pipe(gulp.dest('./web'))
+		.pipe(browserSync.reload({stream: true}))
+		;
+});
+
+gulp.task('default', ['templates', 'server'], function () {
+
+	gulp.watch('./src/templates/**', ['templates']);
+	gulp.watch('./src/app/config/**', ['templates']);
+
+	//gulp.src('./web').pipe(watch('src/js/**/*.*', function () {
+	//	gulp.run('browserify');
+	//}));
+
+});
+
+/**
+ * Function for handing error
+ */
+function handleError(error) {
+	console.error(error);
+	browserSync.notify('<span style="color: red">' + error + '</span>', 3600 * 1000);
+	this.emit('end');
+}
+
+function getDataForFile(file){
+	var config = JSON.parse(fs.readFileSync('./src/app/config/parameters.json'));
+
+	return {
+		app: {
+			config: config
+		},
+		file: file.relative
+	};
+}
